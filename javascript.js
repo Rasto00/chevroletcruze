86bfcf913cb8fa5a8490585b6f259ece0941a9f0
Risto Myllylä <t9myri00@students.oamk.fi>
2020-02-24 01:53:16 +0200

    basic structure

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function pictures()
+    {
+        var div = document.getElementById("pictures");
+ if (div.style.display !== "block") {
+     div.style.display = "block";
+ }
+ else {
+     div.style.display = "none";
+ }
+  }
